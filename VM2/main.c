4b82cbce181a32f8f8b0c99b5ec65a23de434b7e
Daniel Fagnan <dnfagnan@gmail.com>
2012-12-17 19:04:53 -0700

    Initial Commit

---
+++ VM2/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  VM2
+//
+//  Created by Daniel Fagnan on 2012-12-17.
+//  Copyright (c) 2012 Daniel Fagnan. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
