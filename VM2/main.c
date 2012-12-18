0fc84933a6ee67296788cec1e79e8fca0ed77710
Daniel Fagnan <dnfagnan@gmail.com>
2012-12-17 19:32:51 -0700

    removed cpr
    Signed-off-by: Daniel Fagnan <dnfagnan@gmail.com>

--- VM2/main.c
+++ VM2/main.c
@@ -1,11 +1,3 @@
-//
-//  main.c
-//  VM2
-//
-//  Created by Daniel Fagnan on 2012-12-17.
-//  Copyright (c) 2012 Daniel Fagnan. All rights reserved.
-//
-
 #include <stdio.h>
 #include <stdlib.h>
 
